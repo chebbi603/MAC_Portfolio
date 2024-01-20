f20ccd388aa7d7ad6cf4a102108726ff51a7d041
Ayoub Chebbi <53580077+chebbi603@users.noreply.github.com>
2024-01-20 12:02:27 +0100

    https://github.com/chebbi603/MAC_Portfolio

--- src/App.js
+++ src/App.js
@@ -1,44 +1,106 @@
-import logo from './logo.svg';
+import logo from '../../mac_portfolio/src/logo.svg';
 import './App.css';
 import Lenis from "@studio-freight/lenis";
 import Home from "./home";
-import Typekit from "react-typekit/lib/Typekit";
-import {gsap} from "gsap";
+import {gsap} from 'gsap';
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {useEffect} from "react";
 
 function App() {
-  const lenis = new Lenis()
 
-  lenis.on('scroll', (e) => {
-    //console.log(e)
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, (1.001 - Math.pow(2,-10*t)))
   })
 
   function raf(time) {
     lenis.raf(time)
     requestAnimationFrame(raf)
   }
-
   requestAnimationFrame(raf)
 
-  let lt = gsap.timeline({
-    scrollTrigger:{
-      trigger:'.landing-img',
-      start:'top 10%',
-      end:'+=200',
-      scrub:true,
-      markers:true,
-      toggleActions:'repeat pause reverse pause'
-    }
-  })
-  lt.to('.landing-img',{y:-50,duration:8})
+
+
+function shakeitup(classname){
+  document.querySelectorAll(classname).forEach(btn=>{
+    var rect = btn.getBoundingClientRect();
+    btn.addEventListener('mousemove',(e)=>{
+      let x = e.offsetX
+      let y = e.offsetY-window.scrollY
+      let btnWidth = btn.clientWidth;
+      let btnHeigth = btn.clientHeight;
+      let transX = (x-btnWidth/2)/4;
+      let transY = (y-btnHeigth/2)/4;
+      btn.style.transform = `translateX(${transX*.15}px) translateY(${transY*.15 - 50}px) scale(1.05)`
+
+    })
+    btn.addEventListener('mouseout', (e)=>{
+      btn.style.transform=``;
+      window.scrollBy(0, 0.0001);
+    })
+  });
+}
+
+
+  useEffect(() => {
+    // shakeitup('.social-container');
+    gsap.to('.social-container', {
+      scrollTrigger: {
+        trigger: '.social-container',
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      },
+      y: -100, // Adjust the value for the desired effect
+    });
+  gsap.to('.esmi',{
+    scrollTrigger: {
+      trigger: '.esmi',
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true,
+    },
+    y: -100, // Adjust the value for the desired effect
+  });
+    gsap.to('.role', {
+      scrollTrigger: {
+        trigger: '.role',
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      },
+      y: -100, // Adjust the value for the desired effect
+    });
+    gsap.to('.landing-img', {
+      scrollTrigger: {
+        trigger: '.landing-img',
+        start: 'top',
+        end: 'bottom',
+        scrub: true,
+      },
+      y: -100, // Adjust the value for the desired effect
+    });
+
+})
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <Home></Home>
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo} className="App-logo" alt="logo" />
+        <section className={"section1"}>
+          <Home/>
+        </section>
+        <section data-speed={"2"} className={"section2"}>
+          <img src={logo} className="Applogo" alt="logo" />
+          <img src={logo} className="Applogo" alt="logo" />
+          <img src={logo} className="Applogo" alt="logo" />
+          <img src={logo} className="Applogo" alt="logo" />
+          <img src={logo} className="Applogo" alt="logo" />
+          <img src={logo} className="Applogo" alt="logo" />
+        </section>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/home.js
+++ src/home.js
@@ -1,25 +1,50 @@
-import Lenis from "@studio-freight/lenis";
+// noinspection JSVoidFunctionReturnValueUsed
+
 import "./home.css"
 import landingimg from "../src/assets/photo.png";
 import logosmall from "../src/assets/logo_small.svg";
-import {gsap} from "gsap";
+import descstar from "../src/assets/star.svg";
+import fb from "../src/assets/fb.svg";
+import lin from "../src/assets/linkedin.svg";
+import dri from "../src/assets/dribbble.svg";
+
+
 function Home(){
 
-
     return(
-        <div className="Home">
+        <div className="home">
+            <div className={"home-content"}></div>
             <div className="landing-container">
                 <img src={landingimg} className="landing-img" alt={"byMalek"}/>
             </div>
+            <div className={"home-content-data"}>
             <div className={"header"}>
-                <img src={logosmall} className="header-img" alt={"byMalek"}/>
+                <img src={logosmall} className="header-logo" alt={"byMalek"}/>
                 <p className={"header-text"}>Awesomeness</p>
             </div>
-            <div className={"esmi"}>
+            <div className={"content"}>
+            <div className={"esmi"}  >
                 <p className={"esmi-lakab"}>Chebbi</p>
                 <p className={"esmi-esm"}>Mohamed Ayoub</p>
             </div>
+            <div className={"role"} >
+                <img className={"desc-star"} src={descstar}/>
+                <p className={"desc-text"}>Freelance</p>
+                <p className={"desc-role"}></p>
+            </div>
+            <div className={"social-container"} >
+            <div className={"socials"}>
+                <img className={"social-icon"} src={fb}/>
+                <img className={"social-icon"} src={lin}/>
+                <img className={"social-icon"} src={dri}/>
+            </div>
+            </div>
+            </div>
+            </div>
         </div>
     );
+
 }
+
+
 export default Home ;
